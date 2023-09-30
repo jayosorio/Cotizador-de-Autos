9b937212dcc788809ba94151508598080a2030cf
Jay Osorio Web <jayosorio709@gmail.com>
2023-09-30 13:41:16 -0500

    Validar formulario

--- js/app.js
+++ js/app.js
@@ -23,10 +23,56 @@ UI.prototype.llenarOpcines = () => {
     }
 }
 
+// Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div');
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto')
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // Insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+}
+
 // Instanciar UI
 const ui = new UI();
-console.log(ui);
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpcines(); // Llena el select con los años...
 })
+
+// Validar formulario
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value
+
+    // Leer el año seleccionado
+    const year = document.querySelector('#year').value
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value
+
+    if (marca === '' || year === '' || tipo === '') {
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
+    }
+    ui.mostrarMensaje('Cotizando...', 'exito');
+}
